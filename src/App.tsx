a11dcef664ad38fb8c913a7517dc829e6fe73dba
hossein taromi <hosseintaromii@gmail.com>
2023-09-04 15:20:35 +0330

    feat: :sparkles: add change speed button
    we add change speed button to app (main app) and add change speed func to hlspalyer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { useRef } from "react";
 import { HlsPlayer } from "./components/HlsPlayer";
-const App = () => (
-  <div>
-    <HlsPlayer
-      customTheme={{
-        colors: {
-          primary: "yellow",
-        },
-      }}
-    />
-  </div>
-);
+import { ControllerRefType } from "./@types/HlsPlayerType";
+const App = () => {
+  const controllerRef = useRef<ControllerRefType>({
+    changeSpeed: () => {},
+  });
+
+  return (
+    <>
+      <HlsPlayer
+        controllerRef={controllerRef}
+        customTheme={{
+          colors: {
+            primary: "yellow",
+            videoBg: "#000",
+          },
+        }}
+      />
+      <button
+        onClick={() => {
+          controllerRef.current.changeSpeed(4);
+        }}
+      >
+        vlkdnviosnvsdnviosdv
+      </button>
+    </>
+  );
+};
 
 export default App;
--- src/components/HlsPlayer.tsx
+++ src/components/HlsPlayer.tsx
@@ -2,37 +2,88 @@ import { useVideoHls } from "../hooks/useVideoHls";
 import { ThemeProvider } from "@emotion/react";
 import styled from "@emotion/styled";
 import { ThemeType, theme } from "../theme";
+import { RefObject, useCallback, useImperativeHandle } from "react";
+import { ControllerRefType } from "../@types/HlsPlayerType";
 
-const Text = styled.p(({ theme }) => ({
-  color: theme.colors.primary,
+const VideoWrapper = styled.div(({ theme }) => ({
+  height: "100%",
+  width: "100%",
+  position: "relative",
+  overflow: "hidden",
 }));
+const Video = styled.video(({ theme }) => ({
+  width: "100%",
+  height: "100%",
+  backgroundColor: theme.colors.videoBg,
+}));
+const Button = styled.button({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  background: "transparent",
+  border: "3px solid #fff",
+  borderRadius: "50%",
+  color: "white",
+  "& > img": {
+    display: "none",
+  },
+  // padding: "30px",
+});
+export const HlsPlayer = ({
+  customTheme,
+  controllerRef,
+}: {
+  customTheme?: ThemeType;
+  controllerRef?: RefObject<ControllerRefType>;
+}) => {
+  // const { controllerRef } = useControllerContext();
 
-export const HlsPlayer = ({ customTheme }: { customTheme?: ThemeType }) => {
   const src = "https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8";
   const { videoRef, isSupportedPlatform } = useVideoHls({
     src,
   });
 
+  useImperativeHandle(controllerRef, () => ({
+    changeSpeed: handelChangeSpeed,
+  }));
+
+  const handelChangeSpeed = (value: number) => {
+    if (videoRef?.current?.playbackRate) videoRef.current.playbackRate = value;
+  };
+
+  const playClicked = useCallback(() => {
+    if (videoRef?.current?.paused) {
+      videoRef?.current?.play();
+    } else {
+      videoRef?.current?.pause();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ThemeProvider theme={customTheme ? customTheme : theme}>
-      {isSupportedPlatform ? (
-        <video
-          playsInline
-          ref={videoRef}
-          id="main-video"
-          className="m-video videoBackground"
-          controls={true}
-        />
-      ) : (
-        <video
-          playsInline
-          ref={videoRef}
-          src={src}
-          id="main-video"
-          className="m-video videoBackground"
-          controls={true}
-        />
-      )}
+      <VideoWrapper>
+        {isSupportedPlatform ? (
+          <Video
+            playsInline
+            ref={videoRef}
+            id="main-video"
+            className="m-video videoBackground"
+            controls={false}
+          />
+        ) : (
+          <Video
+            playsInline
+            ref={videoRef}
+            src={src}
+            id="main-video"
+            className="m-video videoBackground"
+            controls={false}
+          />
+        )}
+        <Button onClick={playClicked}>play</Button>
+      </VideoWrapper>
     </ThemeProvider>
   );
 };
